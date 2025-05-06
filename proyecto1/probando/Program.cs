99e947190d083a4288d2cfcddb36ec57d1908771
Osmar <osmar.gtz.botello@hotmail.com>
2025-05-05 23:41:11 -0700

    Nuevos cambios

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,3 +1,4 @@
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Platzi!");
 
+string testingConnection = "Server=tcp:localhost,1433;Initial Catalog=master;User ID=sa;Password=your_password;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;";
