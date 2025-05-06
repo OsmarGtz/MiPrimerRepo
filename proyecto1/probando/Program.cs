67fcf3ecf69e71d51925ba1146eb533cdc2e60de
Osmar Gutierrez <osmar.gtz.botello@gmail.com>
2025-05-05 23:34:43 -0700

    nuevos cambios

--- proyecto1/probando/Program.cs
+++ proyecto1/probando/Program.cs
@@ -1,4 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Platzi!");
 
-string stripe_api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"; // Reemplaza con tu clave secreta de Stripe
